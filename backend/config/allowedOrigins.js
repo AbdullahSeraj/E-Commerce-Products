2d8e4ceac868d8b098dd8875a4b8067c593ace18
AbdullahSeraj <106870849+AbdullahSeraj@users.noreply.github.com>
2025-05-13 23:33:23 +0300

    Changes

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,4 +1,7 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://e-commerce-products-front.onrender.com",
+];
 // const allowedOrigins = ["http://localhost:3000", "https://full-stack-e-commerce-products.web.app"];
 
 module.exports = allowedOrigins;
